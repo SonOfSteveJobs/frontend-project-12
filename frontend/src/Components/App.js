fcea1022228b0c873ac13ec5c2a0d82bca52692f
SonOfSteveJobs <evgenev2222@gmail.com>
2023-06-04 10:32:00 +0300

    fixed routing

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,18 +1,31 @@
-import {Route, Routes} from 'react-router';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import React from 'react';
 import Chat from '../Pages/Chat';
 import Login from '../Pages/Login';
 import NotFound from '../Pages/NotFound';
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Chat />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
 
 function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={<Chat />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <RouterProvider router={router} />
         </div>
     );
 }
